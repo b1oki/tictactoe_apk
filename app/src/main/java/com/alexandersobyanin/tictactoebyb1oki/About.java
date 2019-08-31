8a57c12d1808fd96e3b0728b97eb1e213c8d0b92
Alexander Sobyanin <alexander.sobyanin@gmail.com>
2019-09-01 04:07:18 +0500

    Feat: privacy policy for publish

--- app/src/main/java/com/alexandersobyanin/tictactoebyb1oki/About.java
+++ app/src/main/java/com/alexandersobyanin/tictactoebyb1oki/About.java
@@ -2,16 +2,21 @@ package com.alexandersobyanin.tictactoebyb1oki;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.webkit.WebView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class About extends AppCompatActivity {
+    WebView web;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        web = findViewById(R.id.webView);
+        web.loadUrl("file:///android_asset/privacy_policy_tictactoe_b1oki.html");
 
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
